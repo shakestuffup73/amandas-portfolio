d333c489b5f7b0dcfa9e62aca34ac55bcafc9e1c
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-22 01:22:14 -0400

    update images and css

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,16 +3,16 @@ import styles from '../Landing/Landing.module.css'
 const Landing = () => {
   return (
     <>
-    <main className={styles.container}>
-      <div id="brand-image">
-        <img src="assets/images/AmandaRFLogo.png" alt="amanda stern and dog libby in camera preview display"></img>
-        <h1>Software Engineer</h1>
-        <h2>Wilderness Wanderer</h2>
-        <h2>Avid Photographer</h2>
+    <body className={styles.container}>
+      <div id="layout">
+        <header>
+          
+        </header>
+        <div id="brand-image">
+          <img src="assets/images/AmandaRFLogo.png" alt="amanda stern name through camera aperture"></img>
+        </div>
       </div>
-      <div className={styles.intro}>
-      </div>
-    </main>
+    </body>
     </>
   )
 }
