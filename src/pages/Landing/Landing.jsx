80c85c1280de1e7e9b9890eef2a9a26012b7cac6
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-22 18:54:04 -0400

    update css

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,12 +5,14 @@ const Landing = () => {
     <>
     <body className={styles.container}>
       <div id="layout">
-        <header>
-          
-        </header>
         <div id="brand-image">
           <img src="assets/images/AmandaRFLogo.png" alt="amanda stern name through camera aperture"></img>
         </div>
+      </div>
+      <div id="title-div" className={styles}>
+        <h1>Software Engineer</h1>
+        <h1>Photographer</h1>
+        <h1>Wilderness-Wanderer</h1>
       </div>
     </body>
     </>
