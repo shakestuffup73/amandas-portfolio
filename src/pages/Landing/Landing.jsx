096b9ccd4f7692b49c5732408ff647d71b14f477
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-21 23:44:41 -0400

    position leftnav

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         <img src="assets/images/AmandaRFLogo.png" alt="amanda stern and dog libby in camera preview display"></img>
         <h1>Software Engineer  ||  Photographer  || Loyal human to my dog</h1>
       </div>
-      <div>
+      <div className={styles.intro}>
         <h1>Hey there! I'm Amanda, a professional Software Engineer, outdoor adventurer and non-commital photographer.</h1>
       </div>
     </main>
