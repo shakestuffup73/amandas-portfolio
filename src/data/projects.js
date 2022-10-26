a18b0e1428234a8a348b94936a875b398c73970b
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-26 15:48:48 -0400

    update projects page with mapping over projects data

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    title: "Tic-Tac-CatJam",
+    description: "An interactive tic-tac-toe game with music and flashing light effects for the win!",
+    image: "",
+    repositoryLink: "https://github.com/shakestuffup73/ttt-weekend/blob/main/README.md",
+    deploymentLink: "https://tictaccatjam.fly.dev/",  
+  },
+  {
+    title: "Amandalorian",
+    description: "A choose-your-own adventure game inspired by the Disney Series `The Manadalorian`",
+    image: "",
+    respositoryLink: "https://github.com/shakestuffup73/amandalorian/blob/main/ReadME.md",
+    deploymentLink: "https://amandalorian.netlify.app/",
+  },
+  {
+    title: "New England Rock and Ice",
+    description: "A community forum for rock and ice climbers to share climbs, comment on climbs, share their skills, and add climbing partners to their profiles",
+    image: "",
+    respositoryLink: "https://github.com/shakestuffup73/appalachian-mtns/blob/main/README.md",
+    deploymentLink: "https://appalachian-mtns.fly.dev/",
+  },
+  {
+    title: "LAMdb",
+    description: "A secure database for managing pets' veterinary records",
+    image: "",
+    respositoryLink: "Under Construction",
+    deploymentLink: "Coming Soon",
+  }
+]
+
+export default projects
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,20 +1,30 @@
+import projects from '../../data/projects.js'
 import styles from '../Projects/Projects.module.css'
 
 const Projects = () => {
+  console.log('this is projects', projects)
+
+  if (!projects) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <>
-    <div className={styles.pageLayout}>
-      <div className={styles.titleDiv}>
+      <div className={styles.pageLayout}>
         <img src="assets/images/AmandaProjectsLogo.png" alt="aperture with dev projects words"></img>
+      <div className={styles.titleDiv}></div>
+        <div className={styles.gridContainer}>
+          {projects?.map(project => 
+            <div className={styles.one}>
+              <div>{project.image}</div>
+              <h2>{project.title}</h2>
+              <h3>{project.description}</h3>
+              <h3>{project.deploymentLink}</h3>
+              <h3>{project.repositoryLink}</h3>
+            </div>
+          )}
+        </div>
       </div>
-      <div className={styles.gridContainer}>
-        <div className={styles.one}>One</div>
-        <div className={styles.two}>Two</div>
-        <div className={styles.three}>Three</div>
-        <div className={styles.four}>Four</div>
-      </div>
-    </div>
     </>
   )
 }
