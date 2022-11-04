bd757072fba0281e5865c9d29f340bb40a5c1fd9
Amanda Stern <amanda.stern.73@gmail.com>
2022-11-03 22:10:19 -0400

    code pdf viewer

--- src/pages/Resume/Resume2.jsx
+++ src/pages/Resume/Resume2.jsx
@@ -1,6 +1,30 @@
 import styles from '../Resume/Resume2.module.css'
+import { useState } from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+
+
 
 const Resume2 = () => {
+
+  const [numPages, setNumPages] = useState(null)
+  const [pageNumber, setPageNumber] = useState(1)
+
+  function onDocumentLoadSuccess({numPages}){
+    setNumPages(numPages);
+    setPageNumber(1)
+  }
+  
+  function changePage(offSet) {
+    setPageNumber(prevPageNumber => prevPageNumber + offSet);
+  }
+  
+  function changePageBack(){
+    changePage(-1)
+  }
+  
+  function changePageNext(){
+    changePage(1)
+  }
 
   return (
     <>
@@ -9,8 +33,22 @@ const Resume2 = () => {
         <img src="assets/images/AmandaResumeLogo.png" alt="aperture with dev projects words"></img>
       </div>
       <div className={styles.gridContainer}>
-        <div className={styles.one}>One</div>
-      </div>
+        <div className={styles.one}>
+          <Document 
+            file="assets/pdf/AmandaSternResume.pdf"
+            onLoadSuccess={onDocumentLoadSuccess}>
+              <Page pageNumber={pageNumber} />
+          </Document>
+            <p> Page {pageNumber} of {numPages} </p>
+          { pageNumber > 1 && 
+            <button onClick={changePageBack}>Previous Page</button>
+          }
+          {
+            pageNumber < numPages && 
+              <button onClick={changePageNext}>Next Page</button>
+          }
+        </div>
+    </div>
     </div>
     </>
   )
