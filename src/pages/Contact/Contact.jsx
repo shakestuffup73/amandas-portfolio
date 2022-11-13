e82721ec9e828b52cf8024895542c6b9bfd70746
Amanda Stern <amanda.stern.73@gmail.com>
2022-11-13 12:10:52 -0500

    complete emailJS api integration

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,10 @@
 import { useRef } from 'react';
-import emailjs from '@emailjs/browser'
+import emailjs from 'emailjs-com'
 import styles from './Contact.modules.css'
 
-
-const SERVICE_ID = process.env.SERVICE_ID;
-const TEMPLATE_ID = process.env.TEMPLATE_ID;
-const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID
+const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY
 
 const Contact = () => {
 
@@ -16,17 +15,20 @@ const Contact = () => {
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
+      e.target.reset();
   };
 
   return (
     <>
+      <div className={styles.connect}>
+        <h1>Looking to connect? I'd love to hear from you!</h1>
+      </div>
       <form ref={form} onSubmit={sendEmail}>
-        <div className={styles.connect}>
-          <h1>Looking to connect? I'd love to hear from you!</h1>
+        <div className={styles.form}>
         </div>
         <label>Name</label>
         <input type="text" name="user_name" />
