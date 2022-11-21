99f0686c66456b127a1f7a8a43ab822c6c2aa439
Amanda Stern <amanda.stern.73@gmail.com>
2022-11-21 15:37:57 -0500

    create timeline for resume

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LeftNav from './components/LeftNav/LeftNav';
 import Landing from './pages/Landing/Landing'
 import Projects from './pages/Projects/Projects'
 import ProjectDetails from './pages/Projects/ProjectDetails'
-// import Resume2 from './pages/Resume/Resume2';
+import Resume2 from './pages/Resume/Resume2';
 // import Contact from './pages/Contact/Contact'
 import './App.css';
 
@@ -13,6 +13,7 @@ function App() {
       <LeftNav />
       <Routes>
         <Route path='/' element={<Landing />} />
+        <Route path='/resume' element={<Resume2 />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/projects/:projectDetails' element={<ProjectDetails />} />
       </Routes>
--- src/pages/Resume/Resume2.jsx
+++ src/pages/Resume/Resume2.jsx
@@ -1,59 +1,40 @@
 import styles from '../Resume/Resume2.module.css'
-import { useState } from 'react'
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
-import { amandaResume } from '../../pdf/exports.js'
-
 
 const Resume2 = () => {
 
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1)
-  }
-
-  function changePage(offSet) {
-    setPageNumber(prevPageNumber => prevPageNumber + offSet);
-  }
-
-  function changePageBack() {
-    changePage(-1)
-  }
-
-  function changePageNext() {
-    changePage(1)
-  }
-
   return (
-    <div className={styles.pageLayout}>
-      <h1>Resume</h1>
-      <div className={styles.gridContainer}>
-        <div className={styles.one}>
-          <Document
-            file={amandaResume.src}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            <Page
-              pageNumber={pageNumber}
-              width={300}
-            />
-          </Document>
-          <h2> Page {pageNumber} of {numPages} </h2>
-          {pageNumber > 1 &&
-            <button onClick={changePageBack} className={styles.resumeBtn}>Previous Page</button>
-          }
-          {
-            pageNumber < numPages &&
-            <button onClick={changePageNext} className={styles.resumeBtn}>Next Page</button>
-          }
+    <>
+      <div className={styles.container}>
+        <div className={styles.timeline}>
+          <ul>
+            <li>
+              <div className={styles.timelineContent}>
+                <h1>Role 1</h1>
+                <p>Information About Job</p>
+              </div>
+            </li>
+            <li>
+              <div className={styles.timelineContent}>
+                <h1>Role 2</h1>
+                <p>Information About Job</p>
+              </div>
+            </li>
+            <li>
+              <div className={styles.timelineContent}>
+                <h1>Role 3</h1>
+                <p>Information About Job</p>
+              </div>
+            </li>
+            <li>
+              <div className={styles.timelineContent}>
+                <h1>Role 4</h1>
+                <p>Information About Job</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className={styles.techDiv}>
-        <a href={amandaResume.src} download="amandaResume">Download My Resume</a>
-      </div>
-    </div>
+    </>
   )
 }
 
