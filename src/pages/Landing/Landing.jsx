8cd7a086cfbcf96179a51a9ac088052c7ecb652a
Amanda Stern <amanda.stern.73@gmail.com>
2022-11-21 20:32:15 -0500

    update landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,18 +2,16 @@ import styles from '../Landing/Landing.module.css'
 // import { amandaRFLogo_icon } from '../../assets/images/exports'
 import AboutMe from '../AboutMe/AboutMe'
 import Projects from '../Projects/Projects'
-// import Resume from '../Resume/Resume2'
+import Resume from '../Resume/Resume2'
 import Contact from '../Contact/Contact'
 
 const Landing = () => {
   return (
     <div className={styles.container}>
-      {/* <div id="brand-image">
-        <img src={ amandaRFLogo_icon.src } alt={ amandaRFLogo_icon.alt } className={styles.logo}/>
-      </div> */}
-        <AboutMe />
-        <Projects />
-        <Contact />
+      <AboutMe />
+      <Resume />
+      <Projects />
+      <Contact />
     </div>
   )
 }
