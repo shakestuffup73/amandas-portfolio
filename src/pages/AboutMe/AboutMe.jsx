c9c7f9b4fd334dd0c461e6a7f536ff4dbd82f89c
Amanda Stern <amanda.stern.73@gmail.com>
2022-12-26 13:25:40 -0500

    add dad jokes, update css

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,28 @@
 import styles from '../AboutMe/AboutMe.module.css'
 import { Self8_image } from '../../assets/images/exports'
+import axios from 'axios'
 
 const AboutMe = () => {
+  
+  const getNewJoke = async () => {
+    const joke = document.getElementById('joke')
+    const jokeText = await getDadJoke()
+    console.log('this is jokeText:', jokeText)
+    joke.textContent=jokeText
+    console.log('this is joke:', joke)
+  }
+  
+  const getDadJoke = async () => {
+    try {
+      const config = { headers: { Accept: 'application/json' } }
+      const res = await axios.get('https://icanhazdadjoke.com', config)
+      // console.log(res.data.joke)
+      return res.data.joke
+    } catch (error) {
+      console.log('ERROR!: ', error)
+      return "No jokes for you, sorry!"
+    }
+  }
 
   return (
     
@@ -15,9 +36,20 @@ const AboutMe = () => {
         My professional career started with working project management for software development programs in the Air Force. Upon leaving the military, I went on to work outdoor education and became a Park Ranger in the Bureau of Land Management. As a Ranger I worked wildland fire response and collected field data for integration into SQL databases. After a long walk in the woods, I decided to pursue Software Development full-time and made the career-switch by attending an immersive Software Engineering bootcamp through General Assembly.
         <br></br>
         <br></br>
-        After spending almost a decade exploring the states, I've returned to my New England roots. I now spend my free time wandering the woods with my dog, perusing bookshop shelves for hours, staring at the sky and any body of water I happen upon, people-watching under the guise of drinking coffee, and obsessively listening to the music that puts words to my emotions. I'm also a Professional Photographer and appreciate all forms of artistic expression.
+        After spending almost a decade exploring the states, I've returned to my New England roots. I now spend my free time wandering the woods with my dog, perusing bookshop shelves for hours, staring at the sky and any body of water I happen upon, people-watching under the guise of drinking coffee, and obsessively listening to the music that puts words to my emotions. Shamelessly sharing bad dad jokes and even worse puns is another favorite activity, along with spontaneously napping on warm rocks. I'm also a Professional Photographer and appreciate all forms of artistic expression.
         </p>
       </div>
+      <div className={styles.jokeDiv}>
+          <button onClick={getNewJoke} className={styles.button}>How about a bad Dad-Joke?</button>
+          <p className={styles.jokeP} id='joke'></p>
+        {/* <Lottie
+          loop
+          animationData={dinosaur}
+          play
+          style={{ width: 390 }}
+          className={styles.dino}
+        /> */}
+        </div>
     </div>
   )
 }
--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -2,26 +2,16 @@ import styles from './Intro.module.css'
 import Lottie from 'react-lottie-player'
 import arrow from '../../assets/lotties/arrow.json'
 import 'animate.css'
-import dinosaur from '../../assets/lotties/dinosaur.json'
+// import dinosaur from '../../assets/lotties/dinosaur.json'
 // import { Suspense } from 'react' 
 
 const Intro = () => {
+
   return ( 
     <div className={styles.introDiv}>
       <div className={styles.name}>
-        <h1 class="animate__animated animate__fadeInDown">AMANDA STERN</h1>
-        <h3 class="animate__animated animate__fadeInDown">Software Engineer || Wilderness-Wanderer || Loyal Human To My Dog</h3>
-        {/* <Suspense> */}
-          <div>
-          {/* <Lottie
-            loop
-            animationData={dinosaur}
-            play
-            style={{ width: 390 }}
-            className={styles.dino}
-          /> */}
-          </div>
-        {/* </Suspense> */}
+        <h1 className="animate__animated animate__fadeInDown">AMANDA STERN</h1>
+        <h3 className="animate__animated animate__fadeInDown">Software Engineer || Wilderness-Wanderer || Loyal Human To My Dog</h3>
         <h2>Explore My Portfolio</h2>
         <Lottie
           loop
@@ -30,6 +20,13 @@ const Intro = () => {
           style={{ width: 30 }}
         />
       </div>
+        {/* <Lottie
+          loop
+          animationData={dinosaur}
+          play
+          style={{ width: 390 }}
+          className={styles.dino}
+        /> */}
     </div>
   );
 }
